b800e79820928a09f50c3d225b4f7125f98b1a4e
Junyoung Kim <junkim327@gmail.com>
2019-06-06 23:46:46 +0900

    Add 'class' and '__proto__' example codes

--- object-oriented-programming/class.js
+++ object-oriented-programming/class.js
@@ -6,15 +6,27 @@ class Person {
   }
 
   sum() {
-    return (this.first + this.second);
+    return this.first + this.second;
   }
 }
 
-var kim = new Person('Kim', 10, 20);
-kim.sum = function() {
-  return 'kim : ' + (this.first + this.second);
+class PersonPlus extends Person {
+  constructor(name, first, second, third) {
+    super(name, first, second);
+    this.third = third;
+  }
+
+  sum() {
+    return super.sum() + this.third;
+  }
+
+  avg() {
+    return (this.first + this.second + this.third) / 3;
+  }
 }
+
+var kim = new PersonPlus('Kim', 10, 20, 30);
 var lee = new Person('Lee', 20, 20);
 
-console.log('Kim', kim.sum());
-console.log('Lee', lee.sum());
+console.log('kim.avg()', kim.avg());
+console.log('kim.sum()', kim.sum());
---
+++ object-oriented-programming/prototype-inheritance.js
@@ -0,0 +1,13 @@
+var superObj = { superVal : 'super' };
+// var subObj = { subVal : 'sub' };
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+
+console.log('subObj.subVal =>', subObj.subVal);
+console.log('subObj.superVal =>', subObj.superVal);
+
+subObj.superVal = 'sub';
+
+console.log('superObj.superVal =>', superObj.superVal);
+console.log('subObj.superVal =>', subObj.subVal);
